0767449e0291fb3aa8ff067425da1fe63df67b2c
patrick-204 <p.ohara921@gmail.com>
2024-03-23 16:37:17 -0400

    Made an iss module for fetching IP from ipify api. Added some error checking and handling.

---
+++ iss.js
@@ -0,0 +1,36 @@
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  // use request to fetch public IP address from JSON API
+  const url = 'https://api.ipify.org?format=json';
+
+  request(url, (error, response, body) => {
+    // if there's an error print to command line and handle error
+    if (error) {
+      return callback(error, null);
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // take the json string and transform to object
+    const data = JSON.parse(body);
+
+    callback(null, data['ip']);
+
+  });
+};
+
+module.exports = { fetchMyIP };
