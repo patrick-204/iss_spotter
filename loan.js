65e6f71cb4d9aa573e70647eead0ce23cbd039a2
patrick-204 <p.ohara921@gmail.com>
2024-03-25 22:30:37 -0400

    Created new Promise that resokved or rejected depending on parameters given.

--- loan.js
+++ loan.js
@@ -4,16 +4,16 @@ let creditLimit = 140;
 
 /*
  * Input: number of dollars to loan out
- * Returns: Promise of loan which may or may not fulfill, based on remaining credit. 
- * If creditLimit is less than the requested amount, only the remaining limit is loaned out, otherwise the full amount is loaned out. If $0 remain in the limit, the loan request is rejected (error!).
+ * Returns: Promise of loan which may or may not fulfill, based on remaining credit.
+ * If creditLimit is less than the requested amount, only the remaining limit is loaned out,
+ * otherwise the full amount is loaned out. If $0 remain in the limit, the loan request is rejected (error!).
  */
 const loanOut = function(amount) {
   return new Promise((resolve, reject) => {
     if (creditLimit > 0 && creditLimit - amount < 0) {
       resolve(creditLimit);
       creditLimit = 0;
-    }
-    else if (creditLimit <= 0) {
+    } else if (creditLimit <= 0) {
       reject("Insufficient funds");
     } else {
       resolve(creditLimit);
